44a46e10d54e6959c6bdb72db91b4a3e5dc27789
halid <halidduman60@gmail.com>
2024-12-01 13:44:01 +0300

    videoCard-css-added

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,20 +1,44 @@
+import millify from "millify";
+import { useState } from "react";
+
 const VideoCard = ({ video }) => {
   console.log(video);
+  const [isHover, serIsHover] = useState(true);
   return (
-    <div>
-      {/* resim alanı */}
+    <div className="cursor-pointer">
+      {/* Resim alanı */}
       <div>
         <img
+          className="rounded-lg w-full h-full"
           src={
-            video.richThumbnail
+            isHover && video.richThumbnail
               ? video.richThumbnail[0].url
               : video.thumbnail[video.thumbnail.length - 1].url
           }
-          alt=""
+          alt="image"
         />
       </div>
-      {/* video alt detar */}
-      <div></div>
+      {/* Video alt detay */}
+      <div className="flex mt-5">
+        {/* Sol kanal logosu */}
+        <img
+          className="w-14 h-14 rounded-full"
+          src={video?.channelThumbnail && video?.channelThumbnail[0].url}
+          alt="logo"
+        />
+
+        <div className="ml-4 flex flex-col line-clamp-2">
+          {/* Baslık */}
+          <h4 className="font-bold text-lg line-clamp-3">{video?.title}</h4>
+          {/* Kanal adi */}
+          <p className="text-gray-500 ">{video?.channelTitle}</p>
+          {/* izlenme ve zaman bilgisi */}
+          <div className="flex text-sm text-gray-400 mt-2">
+            <p className="mr-4">{millify(video?.viewCount)} Görüntülenme</p>
+            <p>{video?.publishedTimeText}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
